90ea2045fa7ddda03dd9befe5171924c77817c53
aestera <aest3ra@gmail.com>
2024-01-03 00:03:14 +0900

    project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,41 +3,68 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen
-// by writing 'black' in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen by writing
-// 'white' in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
 
-@color
-M = 0
+@SCREEN
+D=A
+@location
+M=D-1
 
-(LOOP)
-    @color
-    D = A
-    @SCREEN
-    M = D
-
+(CHECK)
     @KBD
-    D = M
-
+    D=M
     @BLACK
     D;JGT
-
     @WHITE
     0;JMP
 
 (BLACK)
-    @color   
-    M = -1
-    @LOOP
-    0;JMP   
+    @24576
+    D=M
+    @location
+
+    D=D-M
+    @CHECK
+    D;JEQ
+
+    @location
+
+    A=M
+    M=-1
+
+    @location
+
+    D=M+1
+    @location
+
+    M=D
+
+    @CHECK
+    0;JMP
 
 (WHITE)
-    @color
-    M = 0
-    @LOOP
-    0;JMP   
+    @SCREEN
+    D=A-1
+    @location
 
+    D=D-M
+    @CHECK
+    D;JEQ
+
+    @location
+
+    A=M
+    M=0
+
+    @location
+
+    D=M-1
+    @location
+
+    M=D
+
+    @CHECK
+    0;JMP
